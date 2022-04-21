5a98ecad2ecb4e4dc344b1293d7aa0f9d9b66f45
yangweiqi <yangweiqi@wafersystems.com>
2022-04-21 16:01:55 +0800

    代码初始化

---
+++ first Day 语法初识 变量声明/first.js
@@ -0,0 +1,6 @@
+var message = "Hello World";
+console.log(message);
+var x;
+x = 1; // 运行正确
+x = undefined; // 运行错误
+x = null; // 运行错误
---
+++ first Day 语法初识 变量声明/first.ts
@@ -0,0 +1,8 @@
+const message:string = "Hello World" 
+console.log(message)
+
+let x: number;
+x = 1; // 运行正确
+x = undefined;    // 运行错误
+
+x = null;    // 运行错误
